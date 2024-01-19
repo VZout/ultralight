2972369d16492d1ecf11cae7706c909f1b1640ea
VZout <viktor.zoutman@embark-studios.com>
2024-01-19 17:36:32 +0100

    ureq

--- build.rs
+++ build.rs
@@ -13,47 +13,49 @@ fn main() {
         println!("cargo:rustc-link-lib=WebCore");
         println!("cargo:rustc-link-lib=AppCore");
 
-        // Download WebCore.dll as its to large to upload to crates.io
-        let webcore_out: String = format!("{}/WebCore.dll", std::env::var("OUT_DIR").unwrap());
-        if !std::path::Path::new(&webcore_out).exists() {
-            let webcore_url =
-                "https://github.com/VZout/ultralight/releases/download/v0.1.3/WebCore.dll";
-            std::process::Command::new("powershell")
-                .args([
-                    "Invoke-WebRequest",
-                    "-URI",
-                    webcore_url,
-                    "-OutFile",
-                    &webcore_out,
-                ])
-                .output()
-                .expect("failed to download WebCore.dll");
+        #[cfg(feature = "requires_dll")]
+        {
+            let profile: String = std::env::var("PROFILE").unwrap();
+            let out_dir: String = std::env::var("OUT_DIR").unwrap();
+            let target = format!("{}/../../../../{}/", out_dir, profile);
+            let target = std::path::Path::new(&target);
+            let target = target.canonicalize().unwrap();
+
+            // If `WebCore.dll` is not found, download the archive and unzip it to the target dir.
+            // WebCore.dll is to large to upload to crates.io
+            let webcore_out: String = format!("{}\\WebCore.dll", target.to_str().unwrap());
+            if !std::path::Path::new(&webcore_out).exists() {
+                let mut response = ureq::get(
+                    "https://github.com/VZout/ultralight/releases/download/v0.1.3/WebCore.dll",
+                )
+                .call()
+                .unwrap()
+                .into_reader();
+
+                //use flate2::read::GzDecoder;
+                //let mut tar = GzDecoder::new(response);
+                let mut file = std::fs::File::create(webcore_out).unwrap();
+                std::io::copy(&mut response, &mut file).unwrap();
+            }
+
+            // Copy dll's to executable directory
+            fs_extra::copy_items(
+                &[
+                    format!(
+                        "{}/ultralight-bin/Ultralight.dll",
+                        env!("CARGO_MANIFEST_DIR")
+                    ),
+                    format!(
+                        "{}/ultralight-bin/UltralightCore.dll",
+                        env!("CARGO_MANIFEST_DIR")
+                    ),
+                    format!("{}/ultralight-bin/AppCore.dll", env!("CARGO_MANIFEST_DIR")),
+                ],
+                target.clone(),
+                &fs_extra::dir::CopyOptions::new().skip_exist(true),
+            )
+            .expect("Failed to copy ultralight dlls");
         }
-
-        let profile: String = std::env::var("PROFILE").unwrap();
-        let out_dir: String = std::env::var("OUT_DIR").unwrap();
-        let target = format!("{}/../../../../{}/", out_dir, profile);
-        let target = std::path::Path::new(&target);
-        let target = target.canonicalize().unwrap();
-
-        // Copy dll's to executable directory
-        fs_extra::copy_items(
-            &[
-                format!(
-                    "{}/ultralight-bin/Ultralight.dll",
-                    env!("CARGO_MANIFEST_DIR")
-                ),
-                format!(
-                    "{}/ultralight-bin/UltralightCore.dll",
-                    env!("CARGO_MANIFEST_DIR")
-                ),
-                webcore_out,
-                format!("{}/ultralight-bin/AppCore.dll", env!("CARGO_MANIFEST_DIR")),
-            ],
-            target.clone(),
-            &fs_extra::dir::CopyOptions::new().skip_exist(true),
-        )
-        .expect("Failed to copy ultralight dlls");
 
         #[cfg(feature = "generate_bindings")]
         {
